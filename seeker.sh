58faa1859dd6625b360e312415ce090605c7867e
Leandro Elias <leandro.elias@vianetbrasil.com.br>
2017-09-28 16:46:58 -0300

    seeker

--- seeker.sh
+++ seeker.sh
@@ -6,11 +6,11 @@ do
 	STR=$(tail -1 nohup.out)
 	if [[ $STR == *"Echoing liga to 553192624941"* ]]; then
 	  curl http://localhost:5000/liga
-	  echo "ligado\n"
+	  printf "ligado\n"
 	fi
 
 	if [[ $STR == *"Echoing desliga to 553192624941"* ]]; then
 	  curl http://localhost:5000/desliga
-	  echo "desligado\n"
+	  printf "desligado\n"
 	fi
 done

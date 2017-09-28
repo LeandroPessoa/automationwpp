32ebcbaf732ce4a553669efc086325ce6c05edaf
Leandro Elias <leandro.elias@vianetbrasil.com.br>
2017-09-28 17:22:02 -0300

    correcao em seeker

--- seeker.sh
+++ seeker.sh
@@ -3,7 +3,7 @@
 echo "Running..."
 while :
 do
-	STR=$(tail -1 nohup.out)
+	STR=$(tail -1 output.txt)
 	if [[ $STR == *"Echoing liga to 553192624941"* ]]; then
 	  curl http://localhost:5000/liga
 	  printf "ligado\n"

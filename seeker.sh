0167ffffde615f4d2882656dd9bc5b0c3deeadc5
Leandro Elias <leandro.elias@vianetbrasil.com.br>
2017-09-28 21:35:14 -0300

    seeker

--- seeker.sh
+++ seeker.sh
@@ -1,16 +1,36 @@
 #!/bin/bash          
+echo "Checando se já não estou em execução..."
 
-echo "Running..."
-while :
-do
-	STR=$(tail -1 output.txt)
-	if [[ $STR == *"Echoing liga to 553192624941"* ]]; then
-	  curl http://localhost:5000/liga
-	  printf "ligado\n"
-	fi
 
-	if [[ $STR == *"Echoing desliga to 553192624941"* ]]; then
-	  curl http://localhost:5000/desliga
-	  printf "desligado\n"
-	fi
-done
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo "Flw vlw"
+        rm -f /tmp/myscript.running
+        exit 1
+}
+
+if [[ -f /tmp/myscript.running ]] ; then
+    exit
+fi
+touch /tmp/myscript.running
+
+
+  echo "Running..."
+	while :
+	do
+		STR=$(tail -1 output.txt)
+		if [[ $STR == *"Echoing liga to 553192624941"* ]]; then
+		  curl http://localhost:5000/liga || rm -f /tmp/myscript.running
+		  printf "ligado\n"
+		fi
+
+		if [[ $STR == *"Echoing desliga to 553192624941"* ]]; then
+		  curl http://localhost:5000/desliga || rm -f /tmp/myscript.running
+		  printf "desligado\n"
+		fi
+	done
+
+
+
